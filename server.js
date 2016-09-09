e33a069de85af463a5f1c5717bdda1ced565198b
chrisdinant <chris.dinant@gmail.com>
2016-09-09 09:06:40 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+   res.send('Hello World'); 
+});
+
+var port = Number(process.env.PORT || 8080);
+app.listen(port, function(){
+    console.log("check");
+});
