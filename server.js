87f5b7e482dc306806a09fc852cd2f9a68fd4e2f
chrisdinant <chris.dinant@gmail.com>
2016-09-09 10:22:12 +0000

    tested for firefox and chrome

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-   res.send('Hello World'); 
+   var ipaddress = req.headers['x-forwarded-for'];
+   var language = req.headers['accept-language'];
+   var lansubstr = language.slice(0,language.indexOf(','));
+   var os = req.headers['user-agent'];
+   var ossubstr = os.slice(1+os.indexOf('('), os.indexOf(')'));
+   var resObj = {"ipaddress": ipaddress, "language": lansubstr, "software": ossubstr};
+   
+   res.send(JSON.stringify(resObj));
+   
+    
 });
 
 var port = Number(process.env.PORT || 8080);
